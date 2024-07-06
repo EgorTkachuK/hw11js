9c70a871e7107aa4dd07e68bb44c46a9141aae8a
Yehor Tkachuk <tkachuk.egor2010@gmail.com>
2024-07-06 09:27:34 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+
+// 1
+// let bankAccount = {
+//     ownerName : "Oslo" ,
+//     accountNumber : 1234 ,
+//     balance : 100,
+//     deposit(value) {
+//     this.balance = this.balance + value 
+//     }
+//     ,
+//     withdraw(value) {
+//     this.balance = this.balance - value 
+//     }
+// }
+// bankAccount.deposit(120)
+// bankAccount.withdraw(121)
+// console.log(bankAccount.balance)
+// 2
+// let weather = {
+//     temperature : 5 ,
+//     humidity : "mid" ,
+//     windSpeed :"29 km/h" ,
+//     checking(value){
+//         this.temperature = value 
+//         if (value > 0 ){console.log("температура вижче 0 градусів Цельсія")} else {console.log("температура нижче 0 градусів Цельсія")}
+//     }
+// }
+// weather.checking(0)
+// console.log(weather.temperature)
+// 3
+// let user = {
+// name : "oslo" , 
+// email : "oslo1999@gmail.com",
+// password : "2000", 
+// checking (correctEmial , correctPassword){
+//  if (this.email === correctEmial && this.password === correctPassword )  {console.log("Every thing ok")} else {console.log("something is not okay")}
+// }
+// }
+// user.checking("oslo1999@gmail.com" , "2000")
+// console.log(user.checking)
